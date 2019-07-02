02d41ef8352e07801de5311f4488cda152659669
xMustang <xMustang@foxmail.com>
2019-07-03 00:42:03 +0800

    钩子方法

--- src/main/java/template/ExamPaper.java
+++ src/main/java/template/ExamPaper.java
@@ -1,13 +1,20 @@
 package template;
 
 public abstract class ExamPaper {
-    /**
-     * 模板方法一般设置为final
-     */
-    public final void question() {
-        System.out.println("题目：水中含有氢元素吗？回答是或者不是。");
-        answerQuestion();
-    }
+	/**
+	 * 模板方法一般设置为final
+	 */
+	public final void question() {
+		System.out.println("题目：水中含有氢元素吗？回答是或者不是。");
+		answerQuestion();
+		// 钩子方法
+		hook();
+	}
+	protected abstract void answerQuestion();
 
-    protected abstract void answerQuestion();
+	/**
+	 * 钩子方法
+	 */
+	protected void hook() {
+	}
 }
--- src/main/java/template/Main.java
+++ src/main/java/template/Main.java
@@ -1,10 +1,13 @@
 package template;
 
+/**
+ * 模板模式
+ */
 public class Main {
-    public static void main(String[] args) {
-        ExamPaper examPaperA = new ExamPaperA();
-        examPaperA.question();
-        ExamPaper examPaperB = new ExamPaperB();
-        examPaperB.question();
-    }
+	public static void main(String[] args) {
+		ExamPaper examPaperA = new ExamPaperA();
+		examPaperA.question();
+		ExamPaper examPaperB = new ExamPaperB();
+		examPaperB.question();
+	}
 }
