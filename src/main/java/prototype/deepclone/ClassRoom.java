650dea12eb3b2afbaed243b39ef7a1a181e19825
xMustang <xMustang@foxmail.com>
2019-11-09 16:44:43 +0800

    深拷贝

--- src/main/java/prototype/deepclone/ClassRoom.java
+++ src/main/java/prototype/deepclone/ClassRoom.java
@@ -1,19 +1,10 @@
 package prototype.deepclone;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ClassRoom implements Cloneable {
     private String roomName;
-
-    public ClassRoom(String roomName) {
-        this.roomName = roomName;
-    }
-
-    public String getRoomName() {
-        return roomName;
-    }
-
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
-    }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
