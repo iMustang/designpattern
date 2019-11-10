7508bb49bac8cca628aca266729555054287ec84
xMustang <xMustang@foxmail.com>
2019-11-10 18:44:49 +0800

    设计模式

--- src/main/java/builder/Computer.java
+++ src/main/java/builder/Computer.java
@@ -1,30 +1,11 @@
 package builder;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class Computer {
     private String cpu;
     private String memory;
-
-    public String getCpu() {
-        return cpu;
-    }
-
-    public void setCpu(String cpu) {
-        this.cpu = cpu;
-    }
-
-    public String getMemory() {
-        return memory;
-    }
-
-    public void setMemory(String memory) {
-        this.memory = memory;
-    }
-
-    @Override
-    public String toString() {
-        return "Computer{" +
-                "cpu='" + cpu + '\'' +
-                ", memory='" + memory + '\'' +
-                '}';
-    }
 }
--- src/main/java/builder/Main.java
+++ src/main/java/builder/Main.java
@@ -2,8 +2,8 @@ package builder;
 
 public class Main {
     public static void main(String[] args) {
-        ComputerDirector computerDirector = new ComputerDirector();
-        Computer computer = computerDirector.build("Intel", "Kinston");
+        ComputerDirector director = new ComputerDirector();
+        Computer computer = director.build("Intel", "Kinston");
         System.out.println(computer);
     }
 }
