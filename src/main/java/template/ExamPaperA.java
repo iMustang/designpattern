f7a3781b0bfbeda08d19e32a3360296c6c8fd459
xMustang <xMustang@foxmail.com>
2019-11-17 13:42:13 +0800

    设计模式

--- src/main/java/template/ExamPaperA.java
+++ src/main/java/template/ExamPaperA.java
@@ -1,11 +1,6 @@
 package template;
 
 /**
- * @Title: ExamPaperA
- * @Description:
- * @Author: xMustang
- * @Version: 1.0
- * @create: 2019/6/19 16:14
  */
 public class ExamPaperA extends ExamPaper {
     @Override
--- src/main/java/template/ExamPaperB.java
+++ src/main/java/template/ExamPaperB.java
@@ -1,11 +1,6 @@
 package template;
 
 /**
- * @Title: ExamPaperB
- * @Description:
- * @Author: xMustang
- * @Version: 1.0
- * @create: 2019/6/19 16:15
  */
 public class ExamPaperB extends ExamPaper {
     @Override
